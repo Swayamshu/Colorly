8d820dcc153439f476b39c9b1d8eb30dbe02f27a
Swayamshu Swain <swayamshu2k@gmail.com>
2022-07-09 19:55:03 +0530

    sidebar ui updates

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,35 +1,75 @@
-import { useNavigate } from "react-router";
+import { useNavigate, useLocation } from "react-router";
 import { useAuth } from "../context/auth";
+import { BsStar, BsStarFill, BsPen, BsPenFill, BsShuffle, BsHeart, BsHeartFill } from "react-icons/bs";
 
 const Sidebar = () => {
     let navigate = useNavigate();
-    const { logout } = useAuth();
+    let path = useLocation().pathname;
+    const { logout, token } = useAuth();
 
     return (
         <div className="sidebar">
             <ul className="sidebar-navs">
-                <li onClick={() => navigate("/")}>
-                    Popular
+                <li
+                    onClick={() => navigate("/")}
+                    className={path === "/" ? "selected-sidebar" : ""}
+                >
+                    {path === "/" ?
+                        <BsStarFill className="sidebar-icons" />
+                        :
+                        <BsStar className="sidebar-icons" />
+                    } Popular
                 </li>
-                <li onClick={() => navigate("/Generate")}>
-                    Generate
+                <li
+                    onClick={() => navigate("/Generate")}
+                    className={path === "/Generate" ? "selected-sidebar" : ""}
+                >
+                    {path === "/Generate" ?
+                        <BsPenFill className="sidebar-icons" />
+                        :
+                        <BsPen className="sidebar-icons" />
+                    } Generate
                 </li>
-                <li onClick={() => navigate("/Random")}>
-                    Random
+                <li
+                    onClick={() => navigate("/Random")}
+                    className={path === "/Random" ? "selected-sidebar" : ""}
+                >
+                    <BsShuffle className="sidebar-icons"/> Random
                 </li>
-                <li onClick={() => navigate("/Collection")}>
-                    Collection
+                <li
+                    onClick={() => navigate("/Collection")}
+                    className={path === "/Collection" ? "selected-sidebar" : ""}
+                >
+                    {path === "/Collection" ?
+                        <BsHeartFill className="sidebar-icons" />
+                        :
+                        <BsHeart className="sidebar-icons" />
+                    } Collection
                 </li>
             </ul>
-            <div onClick={() => navigate("/login")}>
-                Login
-            </div>
-            <div onClick={() => {
-                logout();
-                navigate("/login");
-            }}>
-                Logout
-            </div>
+            {/* <div style={{display:"flex", justifyContent:"center"}}>
+                <hr />
+            </div> */}
+            {token ?
+                <button
+                    className="login-logout-btn"
+                    type="submit"
+                    onClick={() => {
+                        logout();
+                        navigate("/login");
+                    }}
+                >
+                    Logout
+                </button>
+                :
+                <button
+                    className="login-logout-btn"
+                    type="submit"
+                    onClick={() => navigate("/login")}
+                >
+                    Login
+                </button>
+            }
         </div>
     )
 };
